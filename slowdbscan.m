8b5bf2100f561c7213f9bf769ee1cc4ff0f9bc90
TianhuiZhao <44165162+TIANHUIZHAO@users.noreply.github.com>
2019-04-21 17:40:47 +0800

    Update slowdbscan.m

--- slowdbscan.m
+++ slowdbscan.m
@@ -1,5 +1,5 @@
 % -------------------------------------------------------------------------
-% Function: [class,type]=slowdbscan(x,k,Eps)
+% Function: [class,type]=slowdbscan(x,k,Eps,dis)
 % -------------------------------------------------------------------------
 % Aim: 
 % Clustering the data with Density-Based Scan Algorithm with Noise (DBSCAN)
@@ -9,6 +9,8 @@
 % k - number of objects in a neighborhood of an object 
 % (minimal number of objects considered as a cluster)
 % Eps - neighborhood radius, if not known avoid this parameter or put []
+% dis - distance type
+% (Euclidean:1, cosine:2, Adjusted cosine:3,Chebychev distance:4)
 % -------------------------------------------------------------------------
 % Output: 
 % class - vector specifying assignment of the i-th object to certain 
@@ -18,8 +20,7 @@
 % -------------------------------------------------------------------------
 % Example of use:
 % x=[randn(30,2)*.4;randn(40,2)*.5+ones(40,1)*[4 4]];
-% [class,type]=slowdbscan(x,5,[])
-% clusteringfigs('slowdbscan',x,[1 2],class,type)
+% [class,type]=slowdbscan(x,5,[],1)
 % -------------------------------------------------------------------------
 % References:
 % [1] M. Ester, H. Kriegel, J. Sander, X. Xu, A density-based algorithm for 
@@ -36,12 +37,16 @@
 % The University of Silesia
 % December 2004
 % http://www.chemometria.us.edu.pl
+% -------------------------------------------------------------------------
+% Updated by TianhuiZhao
+% Xi'an Jiaotong University
+% April 2019
 
-function [class,type]=slowdbscan(x,k,Eps)
+function [class,type]=slowdbscan(x,k,Eps,dis)
 
-[m,n]=size(x);
+[m,~]=size(x);
 
-if nargin<3 | isempty(Eps)
+if nargin<3 || isempty(Eps)
    [Eps]=epsilon(x,k);
 end
 
@@ -52,12 +57,12 @@ no=1;
 touched=zeros(m,1);
 
 for i=1:m
-    if touched(i)==0;
+    if touched(i)==0
        ob=x(i,:);
-       D=dist(ob(2:n),x(:,2:n));
+       D=dist(ob(2:n),x(:,2:n),dis);
        ind=find(D<=Eps);
     
-       if length(ind)>1 & length(ind)<k+1       
+       if length(ind)>1 && length(ind)<k+1       
           type(i)=0;
           class(i)=0;
        end
@@ -67,7 +72,7 @@ for i=1:m
           touched(i)=1;
        end
 
-       if length(ind)>=k+1; 
+       if length(ind)>=k+1 
           type(i)=1;
           class(ind)=ones(length(ind),1)*max(no);
           
@@ -75,12 +80,12 @@ for i=1:m
                 ob=x(ind(1),:);
                 touched(ind(1))=1;
                 ind(1)=[];
-                D=dist(ob(2:n),x(:,2:n));
+                D=dist(ob(2:n),x(:,2:n),dis);
                 i1=find(D<=Eps);
      
                 if length(i1)>1
                    class(i1)=no;
-                   if length(i1)>=k+1;
+                   if length(i1)>=k+1
                       type(ob(1))=1;
                    else
                       type(ob(1))=0;
@@ -126,24 +131,49 @@ Eps=((prod(max(x)-min(x))*k*gamma(.5*n+1))/(m*sqrt(pi.^n))).^(1/n);
 
 
 %............................................
-function [D]=dist(i,x)
+function [D]=dist(i,x,dtype)
 
-% function: [D]=dist(i,x)
+% function: [D]=dist(i,x,dtype)
 %
 % Aim: 
 % Calculates the Euclidean distances between the i-th object and all objects in x	 
 %								    
 % Input: 
 % i - an object (1,n)
-% x - data matrix (m,n); m-objects, n-variables	    
+% x - data matrix (m,n); m-objects, n-variables
+% dtype - distance type
+% (Euclidean:1, cosine:2, Adjusted cosine:3,Chebychev distance:4)
 %                                                                 
 % Output: 
-% D - Euclidean distance (m,1)
-
-
+% D - distance (m,1)
 
 [m,n]=size(x);
-D=sqrt(sum((((ones(m,1)*i)-x).^2)'));
+switch dtype
+    case 1
+        D=sqrt(sum((((ones(m,1)*i)-x).^2),2))';
+    case 2
+        if n >=2
+            D=zeros(1,m);
+            for ik = 1:m
+                D(ik)=1-sum(dot(i,x(ik,:)))/(norm(i)*norm(x(ik,:)));
+            end
+        end
+    case 3
+        if n >= 2
+            D=zeros(1,m);
+            i = i - mean(i);
+            x = x - repmat(mean(x,2),1,n);
+            for ik = 1:m
+                if norm(i)*norm(x(ik,:)) ~= 0
+                    D(ik)=1-sum(dot(i,x(ik,:)))/(norm(i)*norm(x(ik,:)));
+                else
+                    D(ik)=2;
+                end
+            end
+        end
+    case 4
+        D = max(abs(ones(m,1)*i-x),[],2)';
+end
 
 if n==1
    D=abs((ones(m,1)*i-x))';
